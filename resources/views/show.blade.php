e0ea8bbf446c8de65980aff35e3c06c977ae6136
sunstar0817 <sunstar.sk2023@gmail.com>
2022-02-14 12:55:13 +0900

    ブログ投稿の削除

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,6 +11,12 @@
     <body>
         <h1>Blog Name</h1>
          <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+         <form action="/posts/{{ $post->id }}" id="form_delete" method="post">
+             {{ csrf_field() }}
+             {{ method_field('delete')}}
+             <input type="submit" style="display:none">
+             <p class='delete'>[<span onclick="return deletePost(this);">delete</span>]</p>
+         </form>
              <div class='posts'>
                    <h2 class='title'>{{ $post->title }}</h2>
                    <p class='body'>{{ $post->body }}</p>
@@ -19,5 +25,14 @@
              <div class='back'>
                  [<a href='/'>back</a>]
              </div>
+             <script>
+                 function deletePost(e)
+                 {
+                     'use strict';
+                     if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+                         document.getElementById('form_delete').submit();
+                     }
+                 }
+             </script>
     </body>
 </html>
